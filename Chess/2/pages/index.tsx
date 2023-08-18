f755488c8ac8e3cbf3328711ae107a078c3c6247
Enkhabt Dulguuntengis <HP233059@M303-CC0182.local>
2023-08-18 17:32:47 +0800

    new

---
+++ Chess/2/pages/index.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  const oneBlock = 20
+  const boardWidth = 40
+  const boardHeight = 40
+  return (
+    <main className="flex justify-center">
+      <div>
+      
+      </div>
+    </main>
+  )
+}
